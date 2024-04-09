62efff3f13e853ddd75d3d8dcbdef9bd67f42738
Daniil Ubica <volkovd@team.trueconf.com>
2024-04-09 20:38:34 +0300

    Initial Commit

---
+++ media_editor/media_editorApp.swift
@@ -0,0 +1,17 @@
+//
+//  media_editorApp.swift
+//  media_editor
+//
+//  Created by Daniil Ubica on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct media_editorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
