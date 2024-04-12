52d75ffb430d1831f28c35e27b2f040a8a568d50
Daniil Ubica <volkovd@team.trueconf.com>
2024-04-12 08:37:04 +0300

    image manager update

--- media_editor/ImageManager.swift
+++ media_editor/ImageManager.swift
@@ -17,41 +17,51 @@ enum ErrorType {
 
 class ImageManager: ObservableObject {
     
-    @Published var m_errorType: ErrorType = .NoError
-    @Published var m_imagePath: String = ""
-    private var m_image: NSImage? = nil
-    
+    @Published var m_errorType: ErrorType      = .NoError
+    @Published var m_imagePath: String         = ""
+    @Published var m_currFileExtension: String = ""
+    private var m_image: NSImage?              = nil
+     
     var errorType: ErrorType {
-        self.m_errorType
+        m_errorType
     }
     
     var imagePath: String {
         get {
-            self.m_imagePath
+            m_imagePath
         }
         set(newPath) {
-            self.m_imagePath = newPath
-            if self.m_errorType == .NoImageError {
-                self.m_errorType = .NoError
+            if m_errorType == .NoImageError && m_imagePath != newPath {
+                m_errorType = .NoError
             }
+            
+            m_imagePath = newPath
+            m_currFileExtension = URL(fileURLWithPath: m_imagePath).pathExtension
+        }
+    }
+    
+    var currFileFormat: String {
+        get {
+            m_currFileExtension
+        }
+        set(newExtension) {
+            m_currFileExtension = newExtension
         }
     }
     
     var image: NSImage? {
-        if let image = self.m_image {
-            return image
-        }
-        else if !self.m_imagePath.isEmpty {
-            self.m_image = NSImage(contentsOfFile: self.m_imagePath)
-        }
-        return self.m_image
+        loadImage()
+        return m_image
     }
+    
     
     func saveImage() {
         if let image = m_image {
             let panel = NSSavePanel()
             panel.allowedContentTypes = [.image]
             panel.canCreateDirectories = true
+            panel.isExtensionHidden = false
+            panel.nameFieldStringValue = "Untitled.\(m_currFileExtension)"
             panel.begin { response in
                 if response == .OK {
                     if let url = panel.url {
@@ -61,48 +71,43 @@ class ImageManager: ObservableObject {
             }
         }
         else {
-            if !self.m_imagePath.isEmpty {
-                if let image = NSImage(contentsOfFile: self.m_imagePath) {
-                    self.m_image = image
+            if !m_imagePath.isEmpty {
+                if let image = NSImage(contentsOfFile: m_imagePath) {
+                    m_image = image
                 }
                 else {
-                    self.m_errorType = .NoImageError
+                    m_errorType = .NoImageError
                     return
                 }
                 saveImage()
             }
             else {
-                self.m_errorType = .NoImageError
+                m_errorType = .NoImageError
                 return
             }
         }
     }
     
-    private func saveImage(_ image: NSImage, url: URL) {
-        if let image = m_image {
-            guard let imageData = image.tiffRepresentation,
-                  let bitmapImage = NSBitmapImageRep(data: imageData),
-                  let data = bitmapImage.representation(using: .png, properties: [:]) else {
-                self.m_errorType = .GetDataError
-                return
-            }
-            
-            do {
-                try data.write(to: url)
-                self.m_errorType = .NoError
-            } catch {
-                self.m_errorType = .SaveMediaError
-            }
+    private func loadImage() {
+        if !m_imagePath.isEmpty {
+            m_image = NSImage(contentsOfFile: m_imagePath)
         }
-        else {
-            if !self.m_imagePath.isEmpty {
-                self.m_image = NSImage(contentsOfFile: self.m_imagePath)
-                saveImage()
-            }
-            else {
-                self.m_errorType = .NoImageError
-                return
-            }
+    }
+    
+    private func saveImage(_ image: NSImage, url: URL) {
+        guard let imageData = image.tiffRepresentation,
+              let bitmapImage = NSBitmapImageRep(data: imageData),
+              let data = bitmapImage.representation(using: .png, properties: [:]) else {
+            m_errorType = .GetDataError
+            return
+        }
+        
+        do {
+            try data.write(to: url)
+            m_errorType = .NoError
+        } catch {
+            m_errorType = .SaveMediaError
+            return
         }
     }
 }
